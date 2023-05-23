8ae67a71cfe2e358d025a946aad7906d4376ce25
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-05-23 23:05:35 +0500

    quiz updated

--- 03_quiz_app/app.js
+++ 03_quiz_app/app.js
@@ -1,81 +1,71 @@
 const quizData = [{
-        question: "Which of the following is a client site language?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "JavaScript",
-        correct: "d",
-    },
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
 
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Cascading Style Sheet",
-        c: "Jason Object Notation",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
-    {
-        question: "What does CSS stands for?",
-        a: "Hypertext Markup Language",
-        b: "Cascading Style Sheet",
-        c: "Jason Object Notation",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "b",
-    }
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
 ];
 
 let index = 0,
     correct = 0,
     incorrect = 0,
     total = quizData.length;
-    
-
 let questionBox = document.getElementById("questionBox");
 let allInputs = document.querySelectorAll("input[type='radio']")
 
 
-
 const loadQuestion = () => {
-    if (total === index) {
-        return quizEnd();
+    if (total == index) {
+        return endQuiz()
     }
 
-    reset()
+    reset();
     const data = quizData[index]
     questionBox.innerHTML = `(${index + 1}) ${data.question}`
-    allInputs[0].nextElementSibling.innerText = data.a
-    allInputs[1].nextElementSibling.innerText = data.b
-    allInputs[2].nextElementSibling.innerText = data.c
-    allInputs[3].nextElementSibling.innerText = data.d
+    allInputs[0].nextElementSibling.innerText = data.a;
+    allInputs[1].nextElementSibling.innerText = data.b;
+    allInputs[2].nextElementSibling.innerText = data.c;
+    allInputs[3].nextElementSibling.innerText = data.d;
 }
 
-document.querySelector("#but").addEventListener(
-    "click",
-    function() {
-        const data = quizData[index]
-        const ans = getAnswer()
-        if (ans == data.correct) {
-            correct++;
-        } else {
-            incorrect++;
-        }
-        index++;
-        loadQuestion()
-
-        console.log(correct);
-        console.log(incorrect);
+const submit = () => {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans == data.correct) {
+        correct++;
+    } else {
+        incorrect++;
     }
-        
-    )
+    index++;
+    loadQuestion();
+}
 
 
 const getAnswer = () => {
@@ -84,12 +74,11 @@ const getAnswer = () => {
         (input) => {
             if (input.checked) {
                 Ans = input.value;
-                
             }
         }
     )
     return Ans;
-    
+
 }
 
 const reset = () => {
@@ -100,9 +89,9 @@ const reset = () => {
     )
 }
 
-const quizEnd = () => {
-   
-    document.getElementsById("box").innerHTML = "hellow"
+const endQuiz = () => {
+
+    document.getElementsById("box").innerText = "hellow world "
 }
 loadQuestion();
 
