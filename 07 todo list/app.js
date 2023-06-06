3f21a4778b36abdca79e52c9e4ed9f0b6903250b
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-06-06 21:05:39 +0500

    to do list

---
+++ 07 todo list/app.js
@@ -0,0 +1,91 @@
+const addBtn = document.querySelector("#addBtn");
+const main = document.querySelector("#main")
+
+addBtn.addEventListener(
+    "click",
+    function () {
+        addNote()
+    }
+)
+
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes);
+    const data = [];
+    notes.forEach(
+            (note) => {
+                data.push(note.value)
+            }
+        )
+        // console.log(data)
+    if (data.length === 0) {
+        localStorage.removeItem("notes")
+    } else {
+        localStorage.setItem("notes", JSON.stringify(data))
+    }
+}
+
+
+
+// <!-- <div class="note">
+// <div class="tool">
+//     <i class="fa-solid fa-floppy-disk"></i>
+//     <i class="fa-solid fa-trash"></i>
+
+// </div>
+// <textarea></textarea>
+// </div>
+
+const addNote = () => {
+    const note = document.createElement("div")
+    note.classList.add("note")
+    note.innerHTML = `
+    <div class="tool">
+        <i class=" save fa-solid fa-floppy-disk"></i>
+        <i class=" trash fa-solid fa-trash"></i>
+
+    </div>
+    <textarea></textarea>
+    `
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function () {
+            note.remove()
+            saveNotes()
+        }
+    )
+    note.querySelector(".save").addEventListener(
+        "click",
+        function () {
+            saveNotes()
+        }
+    )
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function () {
+            saveNotes()
+        }
+    )
+
+
+    main.appendChild(note);
+    saveNotes();
+}
+
+
+(
+    function() {
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+        if (lsNotes === null) {
+            addNote()
+        } else {
+            lsNotes.forEach(
+                (lsNote) => {
+                    addNote(lsNote)
+                }
+            )
+        }
+
+    }
+)() ;
