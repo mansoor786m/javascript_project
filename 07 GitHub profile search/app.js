78be395f14f2c91eee8c28773b809c29eb747a26
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-06-23 06:50:35 +0500

    complete github profile search

--- 07 GitHub profile search/app.js
+++ 07 GitHub profile search/app.js
@@ -1,17 +1,18 @@
-const searchbox = document.querySelector('search');
+const searchbox = document.querySelector('#search');
 const APIURL = "https://api.github.com/users/";
 const main = document.querySelector("#main");
 
 const getData = async (username) => {
     const responce = await fetch(APIURL + username);
     const data = await responce.json();
+    console.log(data)
     const card = `
     <div class="card">
     <div>
         <img class="avatar" src="${data.avatar_url}" alt="Florin Pop">
     </div>
     <div class="user-info">
-        <h2>${data.name}</h2>
+        <h2>${data.name +"/" +data.login}</h2>
         <p>${data.bio}</p>
 
         <ul class="info">
@@ -27,9 +28,6 @@ const getData = async (username) => {
 </div>
             `
 
-
-    // console.log(responce)
-    
     main.innerHTML = card;
     getrepos(username);
 
@@ -39,22 +37,35 @@ const getrepos = async (username) =>{
     const repos = document.querySelector("#repos")
     const responce = await fetch ( APIURL + username +"/repos");
     const data = await responce.json();
-    data.forEach((item) => {
+    data.forEach(
+        (item) => {
+        
         const a = document.createElement("a")
         a.classList.add("repo")
-        a.href = item.url;
+        a.href = item.html_url;
         a.innerText = item.name;
-        repos.appendChild("a")
+        a.target = "_blank"
+        repos.appendChild(a)
         
     });
-    console.log(data)
+   
 }
 
-getData("mansoor786m");
+
+const submitform = ()=>{
+   
+    console.log(searchbox.value)
+    if (searchbox != ""){
+        getData(searchbox.value);
+        searchbox.value = ""
+    }
+
+    return false;
+}
 
 
 
 
-{/* <a class="repo" href="#" target="_blank">Repo 1</a> */}
-{/* <a class="repo" href="#" target="_blank">Repo 2</a> */}
-{/* <a class="repo" href="#" target="_blank">Repo 3</a> */}
+/* <a class="repo" href="#" target="_blank">Repo 1</a> */
+/* <a class="repo" href="#" target="_blank">Repo 2</a> */
+/* <a class="repo" href="#" target="_blank">Repo 3</a> */
