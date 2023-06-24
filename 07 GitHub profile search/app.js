7e7c8da338d958a1590e5b5fbc44ec7afb13b51d
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-06-24 06:18:04 +0500

    create github profile
    to get data from github profile

--- 07 GitHub profile search/app.js
+++ 07 GitHub profile search/app.js
@@ -1,37 +1,60 @@
-const endDate = "30 May 2023 09:30:00 PM"
+const searchbox = document.querySelector('search');
+const APIURL = "https://api.github.com/users/";
+const main = document.querySelector("#main");
 
-document.getElementById("end-date").innerText = endDate;
-const inputs = document.querySelectorAll("input")
-    // const clock = () => {
+const getData = async (username) => {
+    const responce = await fetch(APIURL + username);
+    const data = await responce.json();
+    const card = `
+    <div class="card">
+    <div>
+        <img class="avatar" src="${data.avatar_url}" alt="Florin Pop">
+    </div>
+    <div class="user-info">
+        <h2>${data.name}</h2>
+        <p>${data.bio}</p>
 
-// }
+        <ul class="info">
+            <li>${data.followers}<strong>Followers</strong></li>
+            <li>${data.following}<strong>Following</strong></li>
+            <li>${data.public_repos}<strong>Repos</strong></li>
+        </ul>
 
-function clock() {
-    const end = new Date(endDate)
-    const now = new Date()
-    const diff = (end - now) / 1000;
+        <div id="repos">
+           
+        </div>
+    </div>
+</div>
+            `
 
 
-    if (diff < 0) return ;
+    // console.log(responce)
+    
+    main.innerHTML = card;
+    getrepos(username);
 
-    // convert into days;
-    inputs[0].value = Math.floor(diff / 3600 / 24);
-    inputs[1].value = Math.floor(diff / 3600) % 24;
-    inputs[2].value = Math.floor(diff / 60) % 60;
-    inputs[3].value = Math.floor(diff) % 60;
 }
 
-// initial call
-clock();
-setInterval( function (){
-    clock()
-    } ,
-     1000
-)
+const getrepos = async (username) =>{
+    const repos = document.querySelector("#repos")
+    const responce = await fetch ( APIURL + username +"/repos");
+    const data = await responce.json();
+    data.forEach((item) => {
+        const a = document.createElement("a")
+        a.classList.add("repo")
+        a.href = item.url;
+        a.innerText = item.name;
+        repos.appendChild("a")
+        
+    });
+    console.log(data)
+}
 
-/**
- *  1 day = 24 hrs
- *  1 hr = 60 mins
- *  60 min = 3600 sec
- */
+getData("mansoor786m");
 
+
+
+
+{/* <a class="repo" href="#" target="_blank">Repo 1</a> */}
+{/* <a class="repo" href="#" target="_blank">Repo 2</a> */}
+{/* <a class="repo" href="#" target="_blank">Repo 3</a> */}
