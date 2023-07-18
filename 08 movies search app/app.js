b86f84ea9d5295b35259d5c540464d2f3ad20e39
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-07-18 06:17:21 +0500

    movies file updated

---
+++ 08 movies search app/app.js
@@ -0,0 +1,16 @@
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+    const movies = document.querySelector('#movie-box')
+
+
+const getMovies = async(url) => {
+    const responce = await fetch(url);
+    const data = await responce.json();
+    console.log(data);
+    
+
+};getMovies(APIURL);
