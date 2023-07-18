4c58ce4449aa996914d48156306d3ff7e2255ff7
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-07-18 06:31:50 +0500

    Update app.js

--- 08 movies search app/app.js
+++ 08 movies search app/app.js
@@ -4,13 +4,24 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-    const movies = document.querySelector('#movie-box')
+const movies = document.querySelector('#movie-box')
 
 
-const getMovies = async(url) => {
+const getMovies = async (url) => {
     const responce = await fetch(url);
+    // console.log(responce);
     const data = await responce.json();
-    console.log(data);
-    
+    showMovies(data)
+}; getMovies(APIURL);
 
-};getMovies(APIURL);
+const showMovies = (data) => {
+
+    data.results.forEach(
+        (item) => {
+        console.log(item);
+
+
+    });
+
+
+}
