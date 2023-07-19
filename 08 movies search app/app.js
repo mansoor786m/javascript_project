2672fa9d724e2a3a8a061e0ff92c4d92f4fe7daf
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-07-19 13:16:19 +0500

    movies

--- 08 movies search app/app.js
+++ 08 movies search app/app.js
@@ -4,7 +4,8 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-const movies = document.querySelector('#movie-box')
+const movies_box = document.querySelector('#movie-box')
+const search = document.querySelector("#search")
 
 
 const getMovies = async (url) => {
@@ -18,10 +19,46 @@ const showMovies = (data) => {
 
     data.results.forEach(
         (item) => {
-        console.log(item);
+            // console.log(item);
+
+            const note = document.createElement("div");
+            note.classList.add("box");
+
+            note.innerHTML = `
+            <img src="${IMGPATH + item.poster_path}" alt="image not found">
+            <div class="overlay">
+                <div class="title">
+                    <h2>${item.title}</h2>
+                    <span>${item.vote_average}</span>
+                </div>
+
+                <div class="overview">
+                    <p>${item.overview}</p>
+                </div>
+            </div>
+            `
+            movies_box.appendChild(note);
 
 
-    });
+        });
 
 
 }
+search.addEventListener(
+    "keyup",
+     (e) => {
+        let search = e.target.value
+        // console.log( search);
+        
+        if(search != ""){
+            console.log(search);
+            
+            getMovies(SEARCHAPI + search)
+        }
+        else{
+            console.log("show movies");
+            
+            // getMovies(APIURL)
+        }
+    }
+)
